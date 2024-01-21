b57e1032678d0d021d4abc8d3f8fa58adf6d6d47
colleeno <colleenaoreilly@gmail.com>
2024-01-21 17:51:41 -0500

    Adds fonts, icon, starts animations, and theme mode

---
+++ src/assets/animations.js
@@ -0,0 +1,34 @@
+import anime from 'animejs';
+
+export const animate = {
+	header () {
+		return anime({
+			targets: 'header h1.headline',
+			opacity: [0, 1],
+			translateY: ['-10%', '0%'],
+			easing: 'easeInOutQuad',
+			duration: 500,
+			delay: 300
+		});
+	},
+	border () {
+    return anime({
+      targets: '.border',
+			opacity: [0, 1],
+      width: ['0%', '100%'],
+      easing: 'easeInOutQuad',
+      duration: 500,
+      delay: 300
+    });
+  },
+	fade (targets) {
+    return anime({
+      targets,
+			opacity: [0, 1],
+			translateY: ['-5%', '0%'],
+      easing: 'easeInOutQuad',
+      duration: 500,
+      delay: 500
+    });
+  }
+}
