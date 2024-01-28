d67062f562a42a8999b84e16fdea8fde81a8d5b0
colleeno <colleenaoreilly@gmail.com>
2024-01-28 16:55:53 -0500

    Small style tweaks and copy updates

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -11,19 +11,18 @@
 			<section class="copy">
 				<p>
 					Welcome! Nice to meet you. I&#39;m Colleen O&#39;Reilly, a software engineer living in Annapolis, MD.
-					My non-traditional journey into engineering has equipped me with a unique perspective and focus on the user experience.
-					I&#39;m passionate about the entire user journey - how they came to a website, what they&#39;re looking for, how they interact with the site, 
+					With a background in design and marketing, I&#39;m passionate about the entire user journey - how they came to a website, 
+					what they&#39;re looking for, how they interact with the site, 
 					and how to make that experience as seamless and enjoyable as possible.
 				</p>
 				<p>
 					I graduated Cum Laude from James Madison University with a degree in Media Arts and Design. I studied basic design principles across 
-					a variety of media formats, including print, web, and video. Creating interactive experiences was always my favorite, and was always drawn 
-					to the idea of a career in Web development or design.
+					a variety of media formats, including print, web, and video.
 				</p>
 				<p>
-					My career started at CustomInk.com, where I honed my attention to detail in the art department. My curiosity to learn more about the
+					My career started at CustomInk.com, where I worked in the art department. My curiosity to learn more about the
 					full customer experience led me to an order coordination role. From there I joined the CRM marketing team, where I learned the ins and outs 
-					of email marketing, segmentation, A/B testing, and got my first taste of HTML and CSS to edit email templates and landing pages.
+					of email marketing, and got my first taste of HTML and CSS in order to edit email templates and landing pages.
 				</p>
 				<p>
 					I then joined LivingSocial, a small startup at the time, where I thrived in a fast-paced environment, gained leadership experience, 
@@ -31,7 +30,7 @@
 					and was hooked. From there I took a full stack immersive course at General Assembly and decided to make the career switch to software engineering.
 				</p>
 				<p>
-					I&#39;m currently working at Framebridge, where I was thrilled at the opportunitity to join several former LivingSocial colleagues. I started as a Front-End
+					I&#39;m currently working at Framebridge, where I have been for the past 5 years. I started as a Front-End
 					engineer, assigned to work with the marketing organization. After about a year working exclusively with marketing, I was asked to move to a full stack software engineering 
 					role with our consumer team, focused on our core site experience. It&#39;s been great to expand my knowledge and experience, working on our Rails backend, 
 					and modernizing our front end (ex: AngularJS to Vue.js). Most recently I played a pivotal role in the migration of the consumer-facing website to Shopify, 
@@ -43,7 +42,7 @@
 					When I&#39;m not working, I love spending time outdoors, visiting Bethany Beach, and spending with my awesome rescue dog Weezer. 
 					If you&#39;d like to connect, please reach out to me on <a href="https://www.linkedin.com/in/colleen-o-reilly-a207579/" target="_blank">LinkedIn</a>,
 					and you can find me on github as <a href="https://github.com/colleeno" target="_blank">colleeno</a>. 
-					You can click <a href="/public/Resume_ColleenOReilly.pdf" download="Resume_Colleen O'Reilly">here</a> to download the hardcopy of my resume. 
+					You can click <a href="/Resume_ColleenOReilly.pdf" download="Resume_Colleen O'Reilly">here</a> to download the hardcopy of my resume. 
 					Thanks for stopping by!
 				</p>
 			</section>
@@ -130,8 +129,12 @@ export default {
 	}
 
 	.border {
-		margin: 40px 0;
+		margin: 30px 0;
 		transform-origin: center;
+
+		@media screen and (min-width: $md) {
+			margin: 40px 0;
+		}
 	}
 
 	.text-content {
@@ -139,10 +142,14 @@ export default {
 		box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
 		height: 100%;
 		margin: 10% auto;
-		max-width: 80%;
+		max-width: 90%;
 		padding: 2em;
 		position: relative;
 		z-index: 2;
+
+		@media screen and (min-width: $md) {
+			max-width: 80%;
+		}
 
 		@media screen and (min-width: $lg) {
 			max-width: 70%;
